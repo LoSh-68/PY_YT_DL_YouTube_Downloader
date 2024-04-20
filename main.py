71d294f74251b61ef35610a7a0d3e6136668fd65
LoSh_68 <LoSh_dev@proton.me>
2024-04-21 00:59:22 +0200

    changed main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from class_PY_YT_DL import PY_YT_DL
 import os
 import json
 from variables import SETTINGS_FILE, JSON_DATA
+import pywinstyles
 
 if not os.path.exists(SETTINGS_FILE):
     with open(SETTINGS_FILE, "w") as file:
@@ -9,4 +10,6 @@ if not os.path.exists(SETTINGS_FILE):
 
 if __name__ == "__main__":
     PY_YT_DL = PY_YT_DL()
+    pywinstyles.apply_style(PY_YT_DL, style="acrylic")
     PY_YT_DL.mainloop()
+
