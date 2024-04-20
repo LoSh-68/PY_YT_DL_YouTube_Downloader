666129c9e8832c4ac385e242b6ce2217eb56bc91
LoSh_68 <LoSh_dev@proton.me>
2024-04-20 21:27:05 +0200

    added gui elements

--- class_settings.py
+++ class_settings.py
@@ -1,5 +1,7 @@
 import customtkinter
 from variables import APPNAME
+
+
 class Settings_Window(customtkinter.CTkToplevel):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -26,13 +28,15 @@ class Settings_Window(customtkinter.CTkToplevel):
         self.miscellaneous_settings_frame = customtkinter.CTkFrame(self, width=500, height=400, corner_radius=40)
         self.miscellaneous_settings_frame.grid(pady=20, padx=20, sticky="n", row=3, column=2)
 
-        self.video_resolution_label = customtkinter.CTkLabel(self.frame_video_settings, text="Resolution:", font=("bahnschrift", 15))
+        self.video_resolution_label = customtkinter.CTkLabel(self.frame_video_settings, text="Resolution:",
+                                                             font=("bahnschrift", 15))
         self.video_resolution_label.grid(pady=20, padx=20, row=0, column=0)
 
         self.video_resolution_combobox = customtkinter.CTkComboBox(self.frame_video_settings)
         self.video_resolution_combobox.grid(pady=20, padx=20, row=0, column=1)
 
-        self.video_filetype_label = customtkinter.CTkLabel(self.frame_video_settings, text="Filetype:", font=("bahnschrift", 15))
+        self.video_filetype_label = customtkinter.CTkLabel(self.frame_video_settings, text="Filetype:",
+                                                           font=("bahnschrift", 15))
         self.video_filetype_label.grid(pady=20, padx=20, row=1, column=0)
 
         self.video_filetype_combobox = customtkinter.CTkComboBox(self.frame_video_settings)
@@ -44,20 +48,58 @@ class Settings_Window(customtkinter.CTkToplevel):
         self.video_fps_combobox = customtkinter.CTkComboBox(self.frame_video_settings)
         self.video_fps_combobox.grid(pady=20, padx=20, row=2, column=1)
 
-        self.video_codec_label = customtkinter.CTkLabel(self.frame_video_settings, text="Video Codec:", font=("bahnschrift", 15))
+        self.video_codec_label = customtkinter.CTkLabel(self.frame_video_settings, text="Video Codec:",
+                                                        font=("bahnschrift", 15))
         self.video_codec_label.grid(pady=20, padx=20, row=3, column=0)
 
         self.video_codec_combobox = customtkinter.CTkComboBox(self.frame_video_settings)
         self.video_codec_combobox.grid(pady=20, padx=20, row=3, column=1)
 
-        self.video_bitrate_label = customtkinter.CTkLabel(self.frame_video_settings, text="Bitrate:", font=("bahnschrift", 15))
+        self.video_bitrate_label = customtkinter.CTkLabel(self.frame_video_settings, text="Bitrate:",
+                                                          font=("bahnschrift", 15))
         self.video_bitrate_label.grid(pady=20, padx=20, row=4, column=0)
 
         self.video_bitrate_combobox = customtkinter.CTkComboBox(self.frame_video_settings)
         self.video_bitrate_combobox.grid(pady=20, padx=20, row=4, column=1)
 
-        self.audio_filetype_label = customtkinter.CTkLabel(self.frame_audio_settings, text="Filetype:", font=("bahnschrift", 15))
+        self.audio_filetype_label = customtkinter.CTkLabel(self.frame_audio_settings, text="Filetype:",
+                                                           font=("bahnschrift", 15))
         self.audio_filetype_label.grid(pady=20, padx=20, row=0, column=0)
 
         self.audio_filetype_combobox = customtkinter.CTkComboBox(self.frame_audio_settings)
         self.audio_filetype_combobox.grid(pady=20, padx=20, row=0, column=1)
+
+        self.audio_codec_label = customtkinter.CTkLabel(self.frame_audio_settings, text="Codec:",
+                                                        font=("bahnschrift", 15))
+        self.audio_codec_label.grid(pady=20, padx=20, row=1, column=0)
+
+        self.audio_codec_combobox = customtkinter.CTkComboBox(self.frame_audio_settings)
+        self.audio_codec_combobox.grid(pady=20, padx=20, row=1, column=1)
+
+        self.audio_bitrate_label = customtkinter.CTkLabel(self.frame_audio_settings, text="Bitrate:",
+                                                          font=("bahnschrift", 15))
+        self.audio_bitrate_label.grid(pady=20, padx=20, row=2, column=0)
+
+        self.audio_bitrate_combobox = customtkinter.CTkComboBox(self.frame_audio_settings)
+        self.audio_bitrate_combobox.grid(pady=20, padx=20, row=2, column=1)
+
+        self.miscellaneous_use_oauth_label = customtkinter.CTkLabel(self.miscellaneous_settings_frame, text="OAUTH:",
+                                                                    font=("bahnschrift", 15))
+        self.miscellaneous_use_oauth_label.grid(pady=20, padx=20, row=0, column=0)
+
+        self.miscellaneous_use_oauth_combobox = customtkinter.CTkComboBox(self.miscellaneous_settings_frame)
+        self.miscellaneous_use_oauth_combobox.grid(pady=20, padx=20, row=0, column=1)
+
+        self.allow_oauth_cache_label = customtkinter.CTkLabel(self.miscellaneous_settings_frame, text="OAUTH Cache:",
+                                                              font=("bahnschrift", 15))
+        self.allow_oauth_cache_label.grid(pady=20, padx=20, row=1, column=0)
+
+        self.allow_oauth_cache_combobox = customtkinter.CTkComboBox(self.miscellaneous_settings_frame)
+        self.allow_oauth_cache_combobox.grid(pady=20, padx=20, row=1, column=1)
+
+        self.win_sound_label = customtkinter.CTkLabel(self.miscellaneous_settings_frame, text="Win Sound",
+                                                      font=("bahnschrift", 15))
+        self.win_sound_label.grid(pady=20, padx=20, row=2, column=0)
+
+        self.win_sound_combobox = customtkinter.CTkComboBox(self.miscellaneous_settings_frame)
+        self.win_sound_combobox.grid(pady=20, padx=20, row=2, column=1)
