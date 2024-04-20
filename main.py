a2a098f8630b29b512df5576eb205132871c2cc9
LoSh_68 <LoSh_dev@proton.me>
2024-04-20 23:47:10 +0200

    added creation of json

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 from class_PY_YT_DL import PY_YT_DL
+import os
+import json
+from variables import SETTINGS_FILE, JSON_DATA
+
+if not os.path.exists(SETTINGS_FILE):
+    with open(SETTINGS_FILE, "w") as file:
+        json.dump(JSON_DATA, file, indent=4)
 
 if __name__ == "__main__":
     PY_YT_DL = PY_YT_DL()
