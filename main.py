aa9439ef6269030820f6b1186b70865e4a152070
LoSh_68 <LoSh_dev@proton.me>
2024-04-21 10:09:50 +0200

    refactor

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from class_PY_YT_DL import PY_YT_DL
 import os
 import json
-from variables import SETTINGS_FILE, JSON_DATA
+from vars_defs import SETTINGS_FILE, JSON_DATA
 import pywinstyles
 
 if not os.path.exists(SETTINGS_FILE):
     with open(SETTINGS_FILE, "w") as file:
         json.dump(JSON_DATA, file, indent=4)
 
+
 if __name__ == "__main__":
     PY_YT_DL = PY_YT_DL()
     pywinstyles.apply_style(PY_YT_DL, style="acrylic")
     PY_YT_DL.mainloop()
-
