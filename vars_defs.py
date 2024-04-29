9b022a21eb865d3e329ce8543cc252dd767983fa
LoSh_68 <LoSh_dev@proton.me>
2024-04-29 21:21:47 +0200

    using def clear_console added def welcome_message

--- vars_defs.py
+++ vars_defs.py
@@ -1,9 +1,24 @@
 import json
 import os
+from colorama import Back, Fore, Style
 
 APPNAME = "PY_YT_DL"
 DOWNLOAD_FOLDER = "PY_YT_DL_DOWNLOADS"
 SETTINGS_FILE = "settings.json"
+
+ASCII_ART = (
+    r"""
+/$$$$$$$  /$$     /$$ /$$     /$$ /$$$$$$$$     /$$$$$$$  /$$      
+| $$__  $$|  $$   /$$/|  $$   /$$/|__  $$__/    | $$__  $$| $$      
+| $$  \ $$ \  $$ /$$/  \  $$ /$$/    | $$       | $$  \ $$| $$      
+| $$$$$$$/  \  $$$$/    \  $$$$/     | $$       | $$  | $$| $$      
+| $$____/    \  $$/      \  $$/      | $$       | $$  | $$| $$      
+| $$          | $$        | $$       | $$       | $$  | $$| $$      
+| $$          | $$        | $$       | $$       | $$$$$$$/| $$$$$$$$
+|__/          |__//$$$$$$ |__/       |__//$$$$$$|_______/ |________/
+                 |______/               |______/ 
+    """
+)
 
 JSON_DATA = [
     {"id": 1, "name": "use_oauth", "content": "False"},
@@ -21,8 +36,25 @@ def get_json_data():
     return json_data
 
 
+def welcome_messsage():
+    print(Fore.BLUE + Back.BLACK + ASCII_ART)
+    print(Fore.BLUE + Back.BLACK + f"Welcome to {APPNAME}\n"
+                                   f"this is the console here you see whats going on.\n")
+    print(Fore.BLUE + Back.BLACK + "you will need the console to use the OAUTH feature\n"
+                                   f"Only with OAUTH you are able to download AGE RESTRICTED content\n"
+                                   f"Simply set it to true in the settings (restart the app) and follow the instruction when downloading AGE RESTRICTED content.\n"
+                                   f"Made by LoSh / https://github.com/LoSh-68/PY_YT_DL_YouTube_Downloader\n")
+    print(Fore.BLACK + Back.LIGHTGREEN_EX + "Green = Good")
+    print(Fore.BLACK + Back.LIGHTRED_EX + "Red = Error")
+    print(Fore.BLACK + Back.LIGHTBLUE_EX + "Blue = Info")
+    print(Style.RESET_ALL)
+
+
+
 def clear_console():
     if os.name == 'nt':
         os.system('cls')
+        welcome_messsage()
     else:
         os.system('clear')
+        welcome_messsage()
